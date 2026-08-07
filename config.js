5d76ab0a6175f4fc91130bb06d8d0c9f03d6c74a
Sr Corsario <116904782+srcorsario@users.noreply.github.com>
2026-08-07 11:17:05 +0200

    Update config.js

--- config.js
+++ config.js
@@ -43,7 +43,7 @@ function getModoAlias(modoInterno) {
 
 // CONFIGURACION ROLAND GARROS (restaurante001)
 const CSV_URL_RESTAURANTE001 = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vT9rPlxpax2lE0rN97c6Hoy_OxUwREqRb48juEBr9C91ZFY2UvaKgC8JdiRcwDrtBErXFVmFRh0Zr5e/pub?gid=0&single=true&output=csv';
-const WEB_APP_URL_RESTAURANTE001 = 'https://script.google.com/macros/s/AKfycbyY13YGeUru6s3MwiuAsrW2B814AS-17wVzgTd8UePTxR2-TV1LdONiRbTkAT7lra6tUA/exec';
+const WEB_APP_URL_RESTAURANTE001 = 'https://script.google.com/macros/s/AKfycbwv3sbEI4MdrwE4x_EsQyAuVvIE7Oj2aXNLMDTsglqi91Zsug4ADN5o2N4F106VjeB0Qw/exec';
 
 // CONFIGURACION US OPEN (restaurante002)
 const CSV_URL_RESTAURANTE002 = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vSOWewZgqWZEFYiIMh8DTUX5tr6EEXBwvUJGr7hrpkCG91UhE5xU8fDJ12qcRVrT69xfZ5NGGGyhNCE/pub?output=csv'; 
